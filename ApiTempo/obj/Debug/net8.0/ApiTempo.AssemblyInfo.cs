ba231c83472f2bfac8a6ed869948715afc121713
ahulle <arthurleitee@gmail.com>
2025-01-27 18:46:29 -0300

    commit

--- ApiTempo/obj/Debug/net8.0/ApiTempo.AssemblyInfo.cs
+++ ApiTempo/obj/Debug/net8.0/ApiTempo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiTempo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9722201f3abe912b3a48e179d7b50d9a6005656")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiTempo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiTempo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
